e8ca357c1bdebb16759f3caa1f53cadedb500fd6
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-13 19:58:14 +0200

    adding solution for linq

--- Practice.LINQ/Practice.LINQ/Program.cs
+++ Practice.LINQ/Practice.LINQ/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Practice.LINQ
+﻿using System.Linq;
+namespace Practice.LINQ
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<User> phoneBook = new List<User>()
+            {
+                new User() { Name = "Lisa", PhoneNumber = "+38(098)3471267" },
+                new User() { Name = "George", PhoneNumber = "+38(098)5472263" },
+                new User() { Name = "Ivan", PhoneNumber = "+7(043)6473427" },
+                new User() { Name = "Anna", PhoneNumber = "+38(066)8472393" },
+                new User() { Name = "Jack", PhoneNumber = "+38(098)5472263" },
+                new User() { Name = "Joe", PhoneNumber = "+38(050)5300260" }
+            };
+            foreach (User user in phoneBook)
+            {
+                Console.WriteLine(user.ToString());
+            }
+
+            var someUser = phoneBook.FirstOrDefault(x => x.PhoneNumber.Contains("+7"));
+            if (someUser != null)
+            {
+                Console.WriteLine($"This is bad guy: {someUser}");
+                phoneBook.Remove(someUser);
+            }
+
+            var j = phoneBook.OrderBy(x => x.Name).Where(x => x.Name.Contains("J")).ToList();
+            if (j != null)
+            {
+                Console.WriteLine(Environment.NewLine + "These are the users you found: ");
+                foreach (User user in j)
+                {
+                    Console.WriteLine(user.ToString());
+                }
+            }
+
+            var value = phoneBook.Select((User x) => x.PhoneNumber.Contains("(098)")).ToList();
+            for (int i = 0; i < value.Count; i++)
+            {
+                if (value[i])
+                {
+                    Console.WriteLine("This is Kyivstar: " + phoneBook[i].ToString());
+                }
+            }
         }
     }
 }
---
+++ Practice.LINQ/Practice.LINQ/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice.LINQ
+{
+    internal class User
+    {
+        public User()
+        {
+            Name = string.Empty;
+            PhoneNumber = string.Empty;
+        }
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public override string ToString()
+        {
+            return Name + " " + PhoneNumber;
+        }
+    }
+}
